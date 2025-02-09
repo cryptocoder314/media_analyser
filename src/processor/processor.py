ab8dc123624aafe80c41ca7a4c3000d92cea9cbb
Crawler M1 <github@magnussao.com>
2025-02-09 12:54:22 -0300

    Adding possibility to edit .mkv metadata before scan

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -1,5 +1,7 @@
 import subprocess
 import json
+import shutil
+from pathlib import Path
 from src.infrastructure.query import (
     get_content_by_name,
     insert_content,
@@ -15,10 +17,24 @@ from src.common.common import (
 )
 from src.common.configuration import get_configuration
 
+FORCED_KEYWORDS = ["forced", "forçada", "forcednarrative"]
+ALLOWED_AUDIO_LANGUAGES = {"japanese", "english", "portuguese"}
+ALLOWED_SUBTITLE_LANGUAGES = {"portuguese", "english"}
+
 
 def process_file(session, file_path):
-    extract_media_info(session, file_path)
+    debug_mode = True if get_configuration('debug_mode') == 'active' else False
+    result = process_and_clean_media_info(file_path, debug_mode)
 
+    if debug_mode:
+        return
+
+    if result:
+        extract_media_info(session, file_path)
+        completed = move_file_to_plex(file_path)
+
+        if completed:
+            print(f"All process completed for: {file_path}")
 
 def extract_media_info(session, file_path):
     result = subprocess.run(
@@ -83,4 +99,85 @@ def extract_and_insert_media_info(session, json_result, file_path, content_id):
                         detect_language(track.get("Language", "")),
                         track.get("Default") == "Yes", track.get("Forced") == "Yes")
 
-    print(f"Processed: {file_path.name}")
+
+def process_and_clean_media_info(file_path, debug_mode):
+    result = subprocess.run([
+        "mediainfo", "--Language=raw", "--Output=JSON", file_path
+    ], capture_output=True, text=True)
+
+    json_result = json.loads(result.stdout) if result.stdout else {}
+
+    track_ids_to_remove = []
+    tracks_to_edit = []
+
+    is_anime = True if "Processing" in str(file_path) and "Anime" in str(file_path) else False
+    default_audio = "japanese" if is_anime else "portuguese"
+    default_subtitle = "portuguese" if is_anime else None
+
+    for track in json_result.get("media", {}).get("track", []):
+        track_id = track.get("UniqueID")
+        track_type = track.get("@type")
+        lang = detect_language(track.get("Language", "und"))
+
+        if track_type == "Audio":
+            print(f"Detected language: {lang} for {track_type}")
+            if lang in ALLOWED_AUDIO_LANGUAGES:
+                edit_params = {"name": lang.capitalize()}
+                if lang == default_audio:
+                    edit_params["flag-default"] = 1
+                tracks_to_edit.append((track_id, edit_params))
+            else:
+                if lang == "unknown" and debug_mode != 'active':
+                    print(f"Language unknown for file: {file_path}")
+                    return False
+                track_ids_to_remove.append(track_id)
+
+        elif track_type == "Text":
+            print(f"Detected language: {lang} for {track_type}")
+            if lang in ALLOWED_SUBTITLE_LANGUAGES:
+                title = lang.capitalize()
+                edit_params = {"name": title}
+
+                if any(keyword in track.get("Title", "").lower() for keyword in FORCED_KEYWORDS):
+                    edit_params["flag-forced"] = 1
+                    edit_params["name"] += " (Forced)"
+
+                if lang == default_subtitle:
+                    edit_params["flag-default"] = 1
+
+                tracks_to_edit.append((track_id, edit_params))
+            else:
+                if lang == "unknown" and debug_mode != 'active':
+                    print(f"Language unknown for file: {file_path}")
+                    return False
+                track_ids_to_remove.append(track_id)
+
+    for track_id, edit_params in tracks_to_edit:
+        mkv_edit_cmd = f"mkvpropedit \"{str(file_path)}\""
+        for param, value in edit_params.items():
+            mkv_edit_cmd += f" --edit track:={track_id} --set {param}=\"{value}\""
+        try:
+            if debug_mode:
+                print("Debug mode active. Will not overwrite file")
+                continue
+
+            subprocess.run(mkv_edit_cmd, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as e:
+            print(f"Error on track {track_id}: {e}")
+            return False
+
+    return True
+
+
+def move_file_to_plex(file_path):
+    new_path_parts = list(file_path.parts)
+    new_path_parts[new_path_parts.index('Processing')] = 'Plex'
+    new_path = Path(*new_path_parts)
+
+    new_path.parent.mkdir(parents=True, exist_ok=True)
+
+    shutil.move(str(file_path), str(new_path))
+
+    if new_path.exists():
+        return True
+    return False
